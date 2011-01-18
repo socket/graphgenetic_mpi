299c7abe385d80202a5142e7e604c6f67f918025
Alexey Streltsow <alexey@zilanetworks.com>
2011-01-18 12:54:35 +0300

    mpi supp

--- main.cpp
+++ main.cpp
@@ -3,20 +3,21 @@
 #include "GGIndividual.h"
 #include "GGPopulation.h"
 
-int main (int argc, char * const argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+#include <mpi.h>
+
+int main (int argc, char **argv) {
+	int taskid = -1;
+	int numtasks = -1;
+	
+	if ( MPI_Init(&argc, &argv) != MPI_SUCCESS ) {
+		cout << "MPI init failed";
+	}
+	else {
+		MPI_Comm_rank(MPI_COMM_WORLD, &taskid);
+		MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
+	}
 	
 	srand(time(NULL));
-	
-	/*vertex_set perm = GG_randperm(10);
-	vertex_set ppl = GG_perm2picklist(perm);
-	vertex_set perm2 = GG_picklist2perm(ppl);
-	
-	GG_printset(perm);
-	GG_printset(ppl);
-	GG_printset(perm2);
-	*/
 	
 	FILE *f = fopen("../../graph.txt", "r");
 	if (!f) {
@@ -25,26 +26,7 @@ int main (int argc, char * const argv[]) {
 	GG_loadMatrixFile(f, GGIndividual::adj_matrix, GGIndividual::adj_matrix_size, GGIndividual::adj_matrix_size);
 	
 	fclose(f);
-	/*
-	double maxfit = 0;
-	
-	for (int i=0; i<100; i++) {
-		GGIndividual *ind = new GGIndividual(GGIndividual::adj_matrix_size);
-		printf("\n\n");
-		GGIndividual::crossoverLP(*ind, ind->mutate()).print();
-		printf("\n");
-		ind->print();
-		maxfit = MAX(ind->fitness(), maxfit);
-	}
-	*/
-	
-	/*GGPopulation gap(11, GGIndividual::adj_matrix_size);
-	
-	for (int i=0; i<200; i++) {
-		gap.print();
-		gap = GGPopulation::generate(gap, 0, 10);
-	}*/
-	
+
 	cout << "Parallel graph partitioning genetic algorithm demonstration\n";
 	cout << "Streltsov A.A.\n\n";
 	
@@ -52,6 +34,8 @@ int main (int argc, char * const argv[]) {
 	g.best_ind().print();
 	
 	cout << "\n";
+	
+	MPI_Finalize();
 	
 	return 0;
 }
