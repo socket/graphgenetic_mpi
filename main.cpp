30ea0be71592b2d7b13e86c8fdf7ed77c700ae97
Alexey Streltsow <alexey@zilanetworks.com>
2011-01-21 20:55:48 +0300

    fixes

--- main.cpp
+++ main.cpp
@@ -22,31 +22,45 @@ int main (int argc, char **argv) {
 	}
 	
 	srand(time(NULL));
+	int msize;
 	
-	FILE *f = fopen("../../graph.txt", "r");
-	if (!f) {
-		f = fopen("graph.txt", "r");
-	}
-	GG_loadMatrixFile(f, GGIndividual::adj_matrix, GGIndividual::adj_matrix_size, GGIndividual::adj_matrix_size);
-	
-	fclose(f);
-
-	if (FILE* ff = fopen("random", "r") ) {
-		char buff[128];
-		fgets(buff, 128, ff);
-		int size = atoi(buff);
-		fclose(ff);
-		if ( size<5) size = 10;
-		
-		GGIndividual::adj_matrix_size = size;
-		GG_genMatrix(GGIndividual::adj_matrix, GGIndividual::adj_matrix_size);
-	}
-		
 	if ( taskid == 0 ) {
 		cout << "Parallel graph partitioning genetic algorithm demonstration\n";
 		cout << "Streltsov A.A.\n\n";
+
+		FILE *f = fopen("../../graph.txt", "r");
+		if (!f) {
+			f = fopen("graph.txt", "r");
+		}
+		GG_loadMatrixFile(f, GGIndividual::adj_matrix, GGIndividual::adj_matrix_size, GGIndividual::adj_matrix_size);
+
+		fclose(f);
+
+		if (FILE* ff = fopen("random", "r") ) {
+			char buff[128];
+			fgets(buff, 128, ff);
+			int size = atoi(buff);
+			fclose(ff);
+			if ( size<5) size = 10;
+			
+			GGIndividual::adj_matrix_size = size;
+			GG_genMatrix(GGIndividual::adj_matrix, GGIndividual::adj_matrix_size);
+		}
+		msize = GGIndividual::adj_matrix_size;
+		MPI_Send(&msize, 1, MPI_INT, 0, 1, MPI_COMM_WORLD);
+		MPI_Send(GGIndividual::adj_matrix, msize*msize, MPI_DOUBLE, 0, 2, MPI_COMM_WORLD);
 	}
-	
+	else {
+		MPI_Recv(&msize, 1, MPI_INT, 0, 1, MPI_COMM_WORLD, NULL);
+		
+		GGIndividual::adj_matrix = (double*)malloc(sizeof(double)*msize*msize);
+		GGIndividual::adj_matrix_size = msize;
+		
+		MPI_Recv(GGIndividual::adj_matrix, msize*msize, MPI_DOUBLE, 0, 2, MPI_COMM_WORLD, NULL);
+		
+	  cout << "#" << taskid << " received problem matrix of size=" << msize << "\n";
+	}
+		
 	GGPopulation g = GG_evolve(100, 50, GGIndividual::adj_matrix_size, 60, 30);
 	
 	if ( taskid == 0 ) {
